7ee73e75b8f12b693303d6eddc88c4ed1a2ca127
Robear Mankaryous <rmankaryous12@gmail.com>
2021-02-22 23:53:16 -0500

    add fire

--- src/pages/[world].svelte
+++ src/pages/[world].svelte
@@ -1,50 +1,26 @@
 <script>
     import { params } from "@roxi/routify";
+    import { onMount } from "svelte";
     const world = $params.world;
 
-    let images = {
-        covid: [
-            "1.png",
-            "2.gif",
-            "3.png",
-            "4.gif",
-            "5.png",
-            "6.png",
-            "7.png",
-            "8.png",
-            "9.png",
-            "10.png",
-            "11.png",
-        ],
-        dday: [
-            "1.png",
-            "2.gif",
-            "3.png",
-            "4.png",
-            "5.png",
-            "6.png",
-            "7.png",
-            "8.png",
-            "9.png",
-            "10.png",
-            "11.png",
-            "12.png",
-            "13.png",
-            "14.png",
-            "15.png",
-            "16.png",
-            "17.png",
-            "18.png",
-            "19.png",
-        ],
-    };
+    let images;
+
+    onMount(async () => {
+        let response = await fetch("config.json");
+        let data = await response.json();
+        images = data[world].images;
+        console.log(images);
+    });
 </script>
 
-<p class="text-4xl my-3">{world.toUpperCase()}</p>
-<div class="masonry lg:mx-36 mx-6 gap-3">
-    {#each images[world] as image}
-        <img class="item" alt={world} src="/worlds/{world}/{image}" />
-    {/each}
+<p class="text-4xl mt-3">{world.toUpperCase()}</p>
+<a class="text-2xl text-blue-500" href="/">&lt; Back</a>
+<div class="masonry lg:mx-24 mx-6 gap-3 mt-3">
+    {#if images}
+        {#each images as image}
+            <img class="item" alt={world} src="/worlds/{world}/{image}" />
+        {/each}
+    {/if}
 </div>
 
 <style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,41 +1,37 @@
 <script>
+    import { onMount } from "svelte";
     import { metatags } from "@roxi/routify";
     metatags.title = "Hostile Environments";
     metatags.description = "An online art gallery...";
 
-    let url = "";
+    let list;
 
-    let list = [
-        {
-            "title": "COVID-19",
-            "folder": "covid",
-            "hero": "1.png"
-        },
-        {
-            "title": "D-DAY",
-            "folder": "dday",
-            "hero": "1.png"
-        },
-    ]
+    onMount(async () => {
+        let response = await fetch("config.json");
+        let data = await response.json();
+        list = data;
+    });
 </script>
 
-<div class="my-3 text-4xl">Hostile Environments</div>
+<div class="my-3 text-4xl">Hostile<br>Environments</div>
 <div class="flex flex-wrap justify-center gap-6 mx-6">
-    {#each list as world}
-        <div class="w-full md:w-1/3 lg:w-1/4">
-            <a href="/{world.folder}">
-                <img
-                    class="rounded-t-lg w-full"
-                    alt="covid"
-                    src="worlds/{world.folder}/{world.hero}"
-                />
-                <div class="bg-gray-600 rounded-b-lg  p-3">
-                    <header class="flex items-center justify-between">
-                        <p class="text-xl">{world.title}</p>
-                        <p class="text-md">02/22/2021</p>
-                    </header>
-                </div>
-            </a>
-        </div>
-    {/each}
+    {#if list}
+        {#each Object.entries(list) as [world, data]}
+            <div class="w-full md:w-1/3 lg:w-1/4">
+                <a href="/{world}">
+                    <img
+                        class="rounded-t-lg w-full"
+                        alt="covid"
+                        src="worlds/{world}/{data.images[0]}"
+                    />
+                    <div class="bg-gray-600 rounded-b-lg  p-3">
+                        <header class="flex items-center justify-between">
+                            <p class="text-xl">{data.name}</p>
+                            <p class="text-md">{data.date}</p>
+                        </header>
+                    </div>
+                </a>
+            </div>
+        {/each}
+    {/if}
 </div>
